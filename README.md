d3ce2a3243f91095f9f3cf235dbbc41d2154e03f
lwy1010 <im.leewy@gmail.com>
2021-06-10 10:23:42 +0800

    init: transfer pkg to 7inch org

---
+++ README.md
@@ -0,0 +1,26 @@
+# @7inch/stylelint-config
+
+## Introduction
+
+A friendly stylelint config preset.
+
+## Install
+
+```sh
+npm i stylelint @7inch/stylelint-config -D
+
+# or with yarn
+yarn add stylelint @7inch/stylelint-config --D
+```
+
+## Usage
+
+create a .stylelintrc.js configuration file in the root of your project:
+
+```js
+module.exports = {
+  extends: ["@7inch/stylelint-config"],
+};
+```
+
+Checkout [Configuration](https://stylelint.io/user-guide/configure) to get more details.
---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = {
+  extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
+
+  // add rule here with comments.
+  rules: {
+    // limit the allowed nesting depth to 3
+    'max-nesting-depth': 3,
+    // disallow empty blocks.
+    'block-no-empty': true,
+    // selectors must be written in lowercase and match the regex
+    'selector-class-pattern': [
+      '^[a-z0-9\\-]+$',
+      {
+        message: 'Selector should be written in lowercase with hyphens (selector-class-pattern)'
+      }
+    ],
+    // disallow units for zero lengths.
+    'length-zero-no-unit': true,
+    // allow 'v-deep' deep selector(for Sass)
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep']
+      }
+    ],
+    // allow keywords of Sass syntax
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'at-root',
+          'content',
+          'debug',
+          'each',
+          'else',
+          'else if',
+          'error',
+          'extend',
+          'for',
+          'forward',
+          'function',
+          'if',
+          'import',
+          'include',
+          'media',
+          'mixin',
+          'return',
+          'use',
+          'warn',
+          'while'
+        ]
+      }
+    ]
+  }
+};
